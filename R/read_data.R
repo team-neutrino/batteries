9a0a4f4316c4d55673c7924f7da13ce0804d0403
Jarad Niemi <niemi@iastate.edu>
2026-03-18 15:56:15 -0500

    Add read_data.R

---
+++ R/read_data.R
@@ -0,0 +1,13 @@
+library("tidyverse")
+library("readxl")
+
+file2025 <- "data/Battery Test results 2024 2025 --.xlsx"
+file2026 <- "data/Battery Test Results 2025-2026 Season.xlsx"
+
+meta  <- read_excel(file2026, sheet = "Metadata") |>
+  select(Battery, Age, Competition)
+
+tests <- read_excel(file2026, sheet = "Battery Test Results") |> 
+  left_join(meta, by = join_by(Battery))
+comps <- read_excel(file2026, sheet = "Competition Log") |> 
+  left_join(meta, by = join_by(Battery))
